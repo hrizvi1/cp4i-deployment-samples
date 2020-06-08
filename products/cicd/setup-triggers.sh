646510ec9609abf81b71e07bac60a311aa3a24d7
Kinshuk Bhardwaj <kinshuk.bhardwaj@ibm.com>
2020-06-08 18:33:48 +0100

    Adding triggers - remove secret creation yaml for role binding: https://github.ibm.com/cp4i/icp4i-sagan/issues/763

--- products/cicd/setup-triggers.sh
+++ products/cicd/setup-triggers.sh
@@ -6,8 +6,6 @@ tkn tb delete --all -f
 tkn tt delete --all -f
 oc delete rolebinding tekton-triggers-rolebinding
 oc delete role tekton-triggers-role
-oc delete secret webhook-secret
 oc delete serviceaccount cicd-ace-sa
-# oc delete serviceaccount tekton-triggers-sa
 
 oc apply -f cicd-webhook-triggers.yaml

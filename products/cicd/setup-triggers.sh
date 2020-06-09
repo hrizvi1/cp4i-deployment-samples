1782226aeb9682afe510d15b11be60cb13e2d2da
Kinshuk Bhardwaj <kinshuk.bhardwaj@ibm.com>
2020-06-09 12:13:31 +0100

    Adding tasks, pipeline and dynamic pipeline resource names: https://github.ibm.com/cp4i/icp4i-sagan/issues/763

--- products/cicd/setup-triggers.sh
+++ products/cicd/setup-triggers.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
-# tkn resource delete --all -f
-tkn el delete --all -f
-tkn tb delete --all -f
-tkn tt delete --all -f
+tkn resource delete --all -f
+tkn tasks delete --all -f
+tkn taskruns delete --all -f
+tkn pipelines delete --all -f
+tkn pipelineruns delete --all -f
+tkn eventlisteners delete --all -f
+tkn triggerbindings delete --all -f
+tkn triggertemplate delete --all -f
 oc delete rolebinding tekton-triggers-rolebinding
 oc delete role tekton-triggers-role
 
